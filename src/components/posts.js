8f1447b0dd42b11fd08c730f66b11ae156d3a91d
simonamarcu2 <supersimy16@gmail.com>
2022-01-25 17:06:42 +0000

    GET request with Fetch

---
+++ src/components/posts.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+
+export default function Posts() {
+  const [post, getPost] = useState([])
+
+  const API = 'https://jsonplaceholder.typicode.com/posts';
+
+  const fetchPost () => {
+    fetch(API)
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res)
+        getPost(res)
+      })
+  }
+
+  useEffect(() => {
+    fetchPost()
+  }, [])
+
+  return (
+    <>
+      <h2>React Fetch Data with REST API Example</h2>
+      <ul>
+        {post.map((item, i) => {
+          return <li key={i}>{item.title}</li>
+        })}
+      </ul>
+    </>
+  )
+}
